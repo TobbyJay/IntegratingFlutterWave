5c582199ebbe890f06b29f613b539cf84990615c
TobbyJay <umohjunior96@gmail.com>
2021-06-01 22:54:43 +0100

    Add project files.

---
+++ IntegratingFlutterwave/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegratingFlutterwave.Models
+{
+    public class Transaction
+    {
+        public Guid TransactionId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public string TransactionReference { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string TransactionStatus { get; set; }
+    }
+}
